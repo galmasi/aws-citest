d3d70166b68bddb3bf8481556087d9ba0f77c229
George Almasi <gheorghe@us.ibm.com>
2023-11-21 19:13:27 +0000

    typo fix in sed
    Signed-off-by: George Almasi <gheorghe@us.ibm.com>

--- bin/config_awscli.sh
+++ bin/config_awscli.sh
@@ -36,6 +36,6 @@ cp config/aws/config ~/.aws
 chmod 0600 ~/.aws/config
 cat config/aws/credentials.in | \
     sed "s/%%AWS_ACCESS_KEY_ID%%/${AWS_ACCESS_KEY_ID}/" | \
-    sed "s/%%AWS_ACCESS_KEY_SECRET%%/${AWS_ACCESS_KEY_SECRET/" > \
+    sed "s^%%AWS_ACCESS_KEY_SECRET%%^${AWS_ACCESS_KEY_SECRET}^" > \
 	~/.aws/credentials
 chmod 0600 ~/.aws/credentials
