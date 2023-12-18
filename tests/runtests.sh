03556131577f370743411576e3ee4cd6f1842ec2
George Almasi <gheorghe@us.ibm.com>
2023-12-18 17:59:42 +0000

    --
    Signed-off-by: George Almasi <gheorghe@us.ibm.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-for test in `find tests -type d`
+passed=0
+failed=0
+total=0
+for test in `find tests -type d | sort`
 do
     echo "RUNNING TEST: ${test}"
     echo "---------------------"
-    ${test}/runtest.sh
+    if ${test}/runtest.sh
+    then
+        passed=$((passed+1))
+    else
+        failed=$((failed+1))
+    fi
+    total=$((total+1))
 done
+
+echo "======================================"
+printf "| Summary: %2d/%2d/%2d total/pass/fail |" ${total} ${passed} ${failed}
+echo "======================================"
+
+exit ${failed}
